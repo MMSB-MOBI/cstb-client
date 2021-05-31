2704e3f309bb11c95d9a41e633257a2ed8ff9486
Cassandre Héritier <cassandre.heritier--tellier@etu.univ-lyon1.fr>
2021-05-31 17:37:36 +0200

    two trees + upload file + submit data

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import "./assets/style.css"
 import router from "./router/router"
-// import Socketio from './plugins/socket.io'
+import Socketio from './plugins/socket.io'
 
 const app = createApp(App);
 app.use(router);
-// app.use(Socketio, {
-//     connection: 'http://localhost:4000',
-//     options: {
+app.use(Socketio, {
+    connection: 'localhost:4000',
+/*     options: {
 //         // Your Socket.io options here
-//     }
-// })
+//     }*/
+})
 app.mount('#app');
--- src/plugins/socket.io.ts
+++ src/plugins/socket.io.ts
@@ -1,8 +1,15 @@
-import { io } from 'socket.io-client'
+import io from 'socket.io-client' // Socket.io-client v2 is package scoped
 
 export default {
   install: (app: any, { connection, options }: any) => {
-    const socket = io(connection, options)
+    console.log("=>", connection, options);
+    const socket = io(connection, options);
+    socket.on("connect_error", (error:any) => {
+      console.log("Connection error", error);
+    });
+    // socket.on("connect", () => {
+    //   console.log("Connection successfull");
+    // });
     app.config.globalProperties.$socket = socket
     app.provide('socket', socket)
   }
