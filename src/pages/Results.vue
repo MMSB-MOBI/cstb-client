a69cc7b93d47f39ac7f0856c53e10a77a6e918f2
Cassandre Héritier <cassandre.heritier@protonmail.com>
2021-06-03 17:06:35 +0200

    results page, component result-page

--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -1,12 +1,11 @@
 <template>
   <div>
-    Results
     <result-page
       v-if="dataLoad"
-      :all-data="data.data_card"
+      :all_data="data.data_card"
       :org_names="data.gi"
-      :complete_data="JSON.striginfy(data.data)"
-      :fasta_metadata="data.fasta_metadata"
+      :complete_data="JSON.stringify(data.data)"
+      :fasta_metadata="JSON.stringify(data.fasta_metadata)"
       :job_tag="data.tag"
       :total_hits="data.number_hits"
       :excluded_names="data.not_in"
@@ -16,24 +15,21 @@
 
 <script lang="ts">
 import { defineComponent, inject, ref } from "vue";
-// import ResultPage from '@mmsb/result_page'
 
 export default defineComponent({
-  // components: { ResultPage },
   setup() {
     const data = ref();
     const dataLoad = ref(false);
     const socket: any = inject("socket");
+
     socket.on("allGenomesResults", (response: any) => {
       data.value = response;
       dataLoad.value = true;
-      console.log(response);
     });
 
     socket.on("specificGeneResults", (response: any) => {
       data.value = response;
       dataLoad.value = true;
-      console.log(response);
     });
 
     return {
