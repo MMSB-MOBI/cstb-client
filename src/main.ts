9e01738d0bf4246b9a396b0f32ccff3a45baffba
Cecile Hilpert <cecile.hilpert@gmail.com>
2022-02-22 14:14:43 +0100

    title and icon

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
+import Vue from 'vue'; 
 import App from './App.vue'
 import router from "./router/router"
 import Socketio from './plugins/socket.io'
 import PrimeVue from '@mmsb/primevue-forked/config';
 import { defineCustomElements } from '@mmsb/result_page/dist/loader';
+
 
 import '@mmsb/primevue-forked/resources/themes/saga-blue/theme.css'
 import '@mmsb/primevue-forked/resources/primevue.min.css'
@@ -12,10 +14,12 @@ import "./assets/style.css"
 
 const app = createApp(App);
 defineCustomElements(window);
+document.title = "CSTB"; 
 app.config.isCustomElement = tag => tag === 'result-page';
 
 app.use(PrimeVue);
 app.use(router);
+
 app.use(Socketio, {
     connection: '/'
 })
